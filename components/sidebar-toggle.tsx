e67c0d9fcbb6b590c8fcef403a2f487e14cdf80b
yihua.cai135 <jess92725@gmail.com>
2025-12-23 15:55:52 +0800

    feat: 新增側邊欄和身分驗證部分中英文切換

--- components/sidebar-toggle.tsx
+++ components/sidebar-toggle.tsx
@@ -1,19 +1,21 @@
-import type { ComponentProps } from 'react';
+import type { ComponentProps } from "react";
 
-import { type SidebarTrigger, useSidebar } from '@/components/ui/sidebar';
+import { type SidebarTrigger, useSidebar } from "@/components/ui/sidebar";
 import {
   Tooltip,
   TooltipContent,
   TooltipTrigger,
-} from '@/components/ui/tooltip';
+} from "@/components/ui/tooltip";
 
-import { SidebarLeftIcon } from './icons';
-import { Button } from './ui/button';
+import { SidebarLeftIcon } from "./icons";
+import { Button } from "./ui/button";
+import { useLocale } from "@/components/LocaleProvider";
 
 export function SidebarToggle({
   className,
 }: ComponentProps<typeof SidebarTrigger>) {
   const { toggleSidebar } = useSidebar();
+  const { t } = useLocale();
 
   return (
     <Tooltip>
@@ -26,7 +28,7 @@ export function SidebarToggle({
           <SidebarLeftIcon size={16} />
         </Button>
       </TooltipTrigger>
-      <TooltipContent align="start">Toggle Sidebar</TooltipContent>
+      <TooltipContent align="start">{t("ui.toggle_sidebar")}</TooltipContent>
     </Tooltip>
   );
 }
